f38099fbc58fe3a25739c4f0cd05a042e8d8584e
ploxan <Rasmus.dufva@outlook.com>
2022-04-05 14:46:41 +0200

    some changes

--- lost-ark/src/components/HeroSection.js
+++ lost-ark/src/components/HeroSection.js
@@ -5,26 +5,33 @@ import './HeroSection.css';
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
-      <div className='hero-btns'>
-        <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
-        >
-          GET STARTED
-        </Button>
-        <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
-        >
-          WATCH TRAILER <i className='far fa-play-circle' />
-        </Button>
+    <div className="hero-container">
+      <h1>Trending Items</h1>
+      <div class="outer">
+        <div class="row">
+          <div class="item">
+            <a>Hi1</a>
+          </div>
+          <div class="item">
+            <a>Hi1</a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="item">
+            <a>Hi1</a>
+          </div>
+          <div class="item">
+            <a>Hi1</a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="item">
+            <a>HI</a>
+          </div>
+          <div class="item">
+            <a>Hi1</a>
+          </div>
+        </div>
       </div>
     </div>
   );
