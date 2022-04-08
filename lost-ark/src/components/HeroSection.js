c5ec338195119182e126cba505e9cbcdf5e60c3e
ploxan <Rasmus.dufva@outlook.com>
2022-04-08 12:30:40 +0200

    Trending items now looks good on phones

--- lost-ark/src/components/HeroSection.js
+++ lost-ark/src/components/HeroSection.js
@@ -7,31 +7,77 @@ function HeroSection() {
   return (
     <div className="hero-container">
       <h1>Trending Items</h1>
-      <div class="outer">
-        <div class="row">
-          <div class="item">
-            <a>Hi1</a>
+      <div class="centered">
+        <section class="cards">
+          <div class="card">
+            <img src="https://lostarkcodex.com/icons/use_6_104.webp" />
+            <p>
+              Guardian Stone Crystal
+              <br />
+              Price:
+              <br />
+              Change:
+            </p>
           </div>
-          <div class="item">
-            <a>Hi1</a>
+
+          <div class="card">
+            <img src="https://lostarkcodex.com/icons/use_7_155.webp" />
+            <p>
+              Honor Leapstone
+              <br />
+              Price:
+              <br />
+              Change:
+            </p>
           </div>
-        </div>
-        <div class="row">
-          <div class="item">
-            <a>Hi1</a>
+
+          <div class="card">
+            <img src="https://lostarkcodex.com/images/icon_quest_0.webp" />
+            <p>
+              Wall familjen
+              <br />
+              Price:
+              <br />
+              Change:
+            </p>
           </div>
-          <div class="item">
-            <a>Hi1</a>
+        </section>
+      </div>
+      <div class="centered">
+        <section class="cards">
+          <div class="card">
+            <img src="https://lostarkcodex.com/icons/use_6_104.webp" />
+            <p>
+              Guardian Stone Crystal
+              <br />
+              Price:
+              <br />
+              Change:
+            </p>
           </div>
-        </div>
-        <div class="row">
-          <div class="item">
-            <a>HI</a>
+
+          <div class="card">
+            <img src="https://lostarkcodex.com/icons/use_7_155.webp" />
+            <p>
+              Honor Leapstone
+              <br />
+              Price:
+              <br />
+              Change:
+            </p>
           </div>
-          <div class="item">
-            <a>Hi1</a>
+
+          <div class="card">
+            <img src="https://lostarkcodex.com/images/icon_quest_0.webp" />
+            <p>
+              Wall familjen
+              <br />
+              Price:
+              <br />
+              Change:
+            </p>
           </div>
-        </div>
+        </section>
       </div>
     </div>
   );
